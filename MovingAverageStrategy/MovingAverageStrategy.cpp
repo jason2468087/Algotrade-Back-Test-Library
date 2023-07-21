d6e84d562e9c658170c6214e480e3c5181257324
DESKTOP-Q7U2VD4\jason <jason2468087@gmail.com>
2023-07-21 21:13:44 +0800

    Fix build bugs

--- MovingAverageStrategy/MovingAverageStrategy.cpp
+++ MovingAverageStrategy/MovingAverageStrategy.cpp
@@ -1,8 +1,10 @@
 ﻿// MovingAverageStrategy.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <iostream>
 #include "ControlPanel.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 ControlPanel testPanel;
 
@@ -24,17 +26,17 @@ void dailyAction(Portfolio& portfolio, ControlPanel* controlPanel, Market& marke
     Price mAvg50{index.movingAverage(controlPanel->getToday(),50,"SPY")};
     Price mAvg5{ index.movingAverage(controlPanel->getToday(),5,"SPY") };
     Price spyPrice{ market.getPrice("SPY") };
-    
-    if (!isEnteredMarket && spyPrice > mAvg50)
+
+    if (!isEnteredMarket && mAvg5 > mAvg50)
     {
         int amount{ portfolio.getCash() / spyPrice };
         controlPanel->buyStock(Order{ MARKET_ORDER,"SPY",amount });
 
         isEnteredMarket = true;
     }
-    else if (isEnteredMarket && spyPrice < mAvg50)
+    else if (isEnteredMarket && mAvg5 < mAvg50)
     {
-        int amount{ portfolio.getShareHolded("SPY")};
+        int amount{ portfolio.getShareHolded("SPY") };
         controlPanel->sellStock(Order{ MARKET_ORDER,"SPY",amount });
 
         isEnteredMarket = false;
@@ -46,7 +48,7 @@ int main()
     testPanel.setDuration(start, end);
 
     testPanel.setInitAction(initAction);
-    testPanel.setDailyTwiceAction(dailyAction);
+    testPanel.setDailyOpenAction(dailyAction);
 
     testPanel.getPortfolioRef().setCash(initWealth);
 
@@ -61,6 +63,8 @@ int main()
 
     testPanel.getPortfolioRef().printPortfolio();
     testPanel.getPortfolioRef().printStatistic();
+    
+    return 0;
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
@@ -73,3 +77,5 @@ int main()
 //   4. 使用 [錯誤清單] 視窗，檢視錯誤
 //   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
 //   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+// AssetBalancePortfolio.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
